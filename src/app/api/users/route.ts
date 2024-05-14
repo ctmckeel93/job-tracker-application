45e034d25e968dd4361b2b7adf58126a377215be
Corey Mckeel <coreyt.mckeel@gmail.com>
2024-05-14 01:47:12 -0400

    updated auth to utilize prisma ORM

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,22 +1,25 @@
 import { NextResponse, NextRequest } from "next/server";
 import {db} from "../index";
 import bcrypt from "bcrypt";
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export async function GET() {
-    const results = await db.query("SELECT * FROM users");
-
-    return NextResponse.json(results[0])
+    const results = await prisma.user.findMany({
+        select: {id:true, email:true, first_name:true, last_name:true}
+    });
+    return NextResponse.json(results)
 }
 
 export async function POST(request: NextRequest) {
     const body = await request.json() ;
-
-    console.log(body)
-    console.log(body.email)
     const newPassword =  await bcrypt.hash(body.password, 10);
-    console.log(newPassword)
+    body.password = newPassword;
 
-    const result = await db.query("INSERT INTO users (first_name, last_name, email, password) VALUES (?,?,?,?);", [body.first_name, body.last_name, body.email, newPassword]);
+    const result = await prisma.user.create({
+        data: body
+    })
 
-    return NextResponse.json("ok");
+    return NextResponse.json(result);
 }
