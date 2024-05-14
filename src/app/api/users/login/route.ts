1e31e9002c6eaf86572f43cea0e610d14c304490
Corey Mckeel <coreyt.mckeel@gmail.com>
2024-05-14 03:17:56 -0400

    updated login prisma code

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,39 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
-import { db } from "../..";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
 
     const body = await request.json();
 
-    const [rows, fields] = await db.query("SELECT * FROM users WHERE email=?", [body.email])
+    const login = await prisma.user.findFirst({
+        where: {email: body.email}
+    })
 
-    const rowArray = rowsAsArray(rows);
-
-    if (rowArray.length < 1) {
-        return NextResponse.json({message: "Invalid login attempt"}, {status: 401})
+    if (login === null) {
+        return NextResponse.json({
+            message: "Invalid login",
+            status: 401
+        })
     }
 
-    const passwordCheck = await bcrypt.compare(body.password, rowArray[0].password);
-
-    if (!passwordCheck) {
-        return NextResponse.json({message: "Invalid login attempt"},{status: 401});
+    if (!await bcrypt.compare(body.password, login.password)) {
+        return NextResponse.json({
+            message: "Invalid login",
+            status: 401
+        })
     }
-
-
-    // request.cookies.set("userId", rowArray[0].id);
     return NextResponse.json({message: "Successful login attempt recorded", data:{
-        id: rowArray[0].id,
-        name: `${rowArray[0].first_name} ${rowArray[0].last_name}`
     }}, {status: 200});
-}
-
-
-const rowsAsArray = (rows:any) => {
-    let rowsArray = [];
-
-    for (let row of rows) {
-        rowsArray.push(row)
-    }
-    return rowsArray;
 }
