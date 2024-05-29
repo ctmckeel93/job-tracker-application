81893e065b1fe6f25a967275f87e240e96461acf
Corey Mckeel <coreyt.mckeel@gmail.com>
2024-05-29 18:44:38 -0400

    styled details page and notes below

--- src/app/(main)/jobs/[id]/page.tsx
+++ src/app/(main)/jobs/[id]/page.tsx
@@ -4,7 +4,8 @@ import { FormEvent, useEffect, useState } from "react";
 import { type JobData } from "../../../../types";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import deleteButton from '../../../../../public/delete-button.svg'
+import deleteButton from "../../../../../public/delete-button.svg";
+import editButton from "../../../../../public/edit-button.svg";
 import { API_URL } from "@/constants";
 import Image from "next/image";
 import Loading from "@/app/components/Spinner";
@@ -14,17 +15,17 @@ export default function JobDetailsPage({ params }: { params: { id: string } }) {
     const [addingNote, setAddingNote] = useState(false);
 
     useEffect(() => {
-        axios.get(`${API_URL}/jobs/${params.id}`)
-            .then(response => setJob(response.data.job))
-            .catch(err => console.log(err))
-    },[])
+        axios
+            .get(`${API_URL}/jobs/${params.id}`)
+            .then((response) => setJob(response.data.job))
+            .catch((err) => console.log(err));
+    }, []);
     const [note, setNote] = useState({
         context: "",
         category: "",
         jobId: Number(params.id),
     });
     const router = useRouter();
-
 
     const removeJob = () => {
         axios
@@ -36,74 +37,80 @@ export default function JobDetailsPage({ params }: { params: { id: string } }) {
     const handleAddNote = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        const formData = new FormData(event.currentTarget)
+        const formData = new FormData(event.currentTarget);
 
         const note = {
             context: formData.get("context"),
             category: formData.get("category"),
             jobId: Number(params.id),
-        }
-        axios.post(API_URL + `notes`, note)
-        .then(() => {
-            setAddingNote(false)
-            setJob(job != null ? {...job, notes: [...job.notes, note]}: null)
-        })
-        .catch(error => console.log(error))
-    }
+        };
+        axios
+            .post(API_URL + `notes`, note)
+            .then(() => {
+                setAddingNote(false);
+                setJob(
+                    job != null ? { ...job, notes: [...job.notes, note] } : null
+                );
+            })
+            .catch((error) => console.log(error));
+    };
 
     if (!job) {
-        return <Loading/>;
+        return <Loading />;
     }
 
     return (
-        <div className="text-white p-4">
-            <div className="flex w-full justify-center items-center gap-2">
-                <h1 className="text-3xl text-center">{job.company}</h1>
-                    <Image onClick={removeJob} className="mt-2" src={deleteButton} alt="delete button" />
+        <div className="text-white p-4 flex flex-col gap-[100px]">
+            <div>
+                <div className="flex w-full justify-center items-center gap-2">
+                    <h1 className="text-3xl text-center">{job.company}</h1>
+                    <Image
+                        onClick={removeJob}
+                        className="mt-2"
+                        src={deleteButton}
+                        alt="delete button"
+                    />
+                    <Link href={`/jobs/${params.id}/update`} className="mt-2">
+                        <Image src={editButton} alt="edit button" />
+                    </Link>
+                </div>
+                <h2 className="text-center text-2xl">{job.position} </h2>
             </div>
-            <h2>Position | {job.position} </h2>
-            <Link
-                href={`/jobs/${params.id}/update`}
-                className="btn btn-warning"
-            >
-                Update
-            </Link>
 
             {addingNote ? (
-                <form onSubmit={handleAddNote} className="w-50 bg-dark mx-auto flex flex-col items-center justify-center p-5 text-white text-left">
-                        <label className="align-self-start mx-2 mb-2" htmlFor="category">
-                            Category:
-                        </label>
+                <form
+                    onSubmit={handleAddNote}
+                    className=" w-full bg-dark mx-auto flex flex-col gap-3 text-white text-left md:w-[50%]"
+                >
+                    <label htmlFor="category">Category:</label>
                     <div className="w-full">
-                        <input
-                            className="w-full m-2"
-                            type="text"
-                            name="category"
-                        />
+                        <input className="w-full text-black" type="text" name="category" />
                     </div>
                     <div className="w-full">
-                        <label htmlFor="note" className="mx-2">Note:</label>
-                        <textarea
-                            className="w-full m-2"
-                            name="context"
-                        ></textarea>
+                        <label htmlFor="note">Note:</label>
+                        <textarea className="w-full text-black" name="context"></textarea>
                     </div>
-                    <button className="btn btn-success align-self-end">Add</button>
+                    <button className="bg-custom-yellow p-4 w-full">Add</button>
                 </form>
             ) : (
-                <button onClick={() => setAddingNote(true)}>Add Note</button>
+                <button
+                    className="bg-custom-yellow w-full p-4"
+                    onClick={() => setAddingNote(true)}
+                >
+                    Add Note
+                </button>
             )}
-            <div>
-                {job && job.notes?.map((note) => (
-                    <>
-                        <div>
-                            <i>{note.created_at}</i>
-                            <p>
-                                {note.context}
-                            </p>
-                        </div>
-                    </>
-                ))}
+            <h2 className="text-center text-2xl">Notes</h2>
+            <div className="flex gap-4 flex-wrap justify-center">
+                {job &&
+                    job.notes?.map((note) => (
+                        <>
+                            <div className="w-[300px] h-[300px] p-4 bg-custom-yellow text-black flex items-center justify-center text-black">
+                                <i>{note.createdAt}</i>
+                                <p>{note.context}</p>
+                            </div>
+                        </>
+                    ))}
             </div>
         </div>
     );
